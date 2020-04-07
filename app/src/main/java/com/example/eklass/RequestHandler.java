fdc382298dd9925dc9bb44bec281a52232507d2d
Sandeep2314 <CollegeManager@gmail.com>
2020-04-07 23:00:15 +0530

    added post

--- app/src/main/java/com/example/eklass/RequestHandler.java
+++ app/src/main/java/com/example/eklass/RequestHandler.java
@@ -1,8 +1,15 @@
 package com.example.eklass;
 
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -13,7 +20,7 @@ public class RequestHandler
     // this method will send the post request to server
     // in the hash ma we have data in key value form
 
-    public String sendPostRequest(String requestURL, HashMap<String, String> params) throws MalformedURLException {
+    public String sendPostRequest(String requestURL, HashMap<String, String> postDataparams) throws MalformedURLException {
         URL url;
 
         StringBuilder sb = new StringBuilder();
@@ -26,6 +33,37 @@ public class RequestHandler
 
             conn.setReadTimeout(15000);
             conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStream os = conn.getOutputStream();
+
+            BufferedWriter writer  = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+
+
+            writer.write(getPostDataString(postDataparams));
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+
+            if(responseCode == HttpsURLConnection.HTTP_OK)
+            {
+                BufferedReader br = new BufferedReader
+                                (new InputStreamReader(conn.getInputStream()));
+
+                sb = new StringBuilder();
+
+                String response;
+
+                while ((response = br.readLine()) != null )
+                {
+                    sb.append(response);
+                }
+            }
 
 
         }
@@ -35,8 +73,41 @@ public class RequestHandler
         }
 
 
-        return "";
+        return sb.toString();
 
+    }
+
+    // this method converts HashMaps into query string
+    private String getPostDataString(HashMap<String, String> params)
+    {
+        StringBuilder result = new StringBuilder();
+
+        boolean first = true;
+
+        for(HashMap.Entry<String, String> entry : params.entrySet())
+        {
+            if(first)
+                first=false;
+            else
+                result.append("&");
+
+            try {
+
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+
+                }
+            catch (UnsupportedEncodingException e)
+                {
+                    e.printStackTrace();
+                }
+
+
+        }
+
+
+        return result.toString();
     }
 
 }
