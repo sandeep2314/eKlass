fd23de8ed721a8bd4d1f1f3c95235aa0d2769cab
Sandeep2314 <CollegeManager@gmail.com>
2020-04-10 19:27:23 +0530

    bus attendance working

---
+++ app/src/main/java/com/example/eklass/BusAttendanceActivity.java
@@ -0,0 +1,37 @@
+package com.example.eklass;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusAttendanceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busattendance);
+
+        List<Attendance> attendancesList;
+        RecyclerView recyclerView;
+        recyclerView = findViewById(R.id.rvSchoolAttendance);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        attendancesList = new ArrayList<>();
+
+        attendancesList.add(new Attendance("Your Child has boarded the Bus"));
+        attendancesList.add(new Attendance("Your child has debaorded the Bus"));
+
+        //DashboardAdapter dashboardAdapter = new DashboardAdapter(this, featureList);
+
+        SchoolAttendanceAdapter schoolAttendanceAdapter = new SchoolAttendanceAdapter(this, attendancesList);
+
+        recyclerView.setAdapter(schoolAttendanceAdapter);
+    }
+}
