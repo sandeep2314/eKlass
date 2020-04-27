dfe04c25351bd46d26c42a36c3e7094e87c57581
Sandeep2314 <CollegeManager@gmail.com>
2020-04-27 14:43:41 +0530

    added KrishnaDB and ScholarDB balance

--- sql/BalanceSMS.sql
+++ sql/BalanceSMS.sql
@@ -1,5 +1,5 @@
 --select * from tblUserInfo
--- select * from tblSMSPurchased
+-- select * from ScholarDB1920.dbo.tblSMSPurchased
 
 DECLARE @FDT DATETIME
 DECLARE @TDT DATETIME
@@ -15,16 +15,64 @@ SELECT t1.userID, 'Fatherson' School
               FROM 
              ( 
               select userID, SUM(SmsPurchasedCount) Purchased 
-              FROM tblSMSPurchased p
+              FROM AccountsDB.DBO.tblSMSPurchased p
 			  WHERE p.CreatedAt BETWEEN @FDT AND @TDT	
               GROUP BY p.UserID 
               ) T1  
               LEFT OUTER JOIN  
               ( 
               select userID, SUM(msgCount) SMSSent 
-              FROM tblSMSSent s
+              FROM AccountsDB.DBO.tblSMSSent s
 			  WHERE s.CreatedAt BETWEEN @FDT AND @TDT	
               GROUP BY s.UserID 
               ) T2 ON t2.userId = t1.UserID 
-              WHERE t1.USerID = 9;
+              WHERE t1.USerID = 9
+
+UNION
+
+-- select * from KrishnaDB1920.dbo.tblUserInfo
+
+SELECT t1.userID, 'KrishnaDB1920' School
+, purchased
+, IsNull(smsSent, 0) SMSSent 
+, T1.Purchased - IsNull(smsSent, 0) as BalanceOnHand 
+              FROM 
+             ( 
+              select userID, SUM(SmsPurchasedCount) Purchased 
+              FROM KrishnaDB1920.DBO.tblSMSPurchased p
+			  WHERE p.CreatedAt BETWEEN @FDT AND @TDT	
+              GROUP BY p.UserID 
+              ) T1  
+              LEFT OUTER JOIN  
+              ( 
+              select userID, SUM(msgCount) SMSSent 
+              FROM KrishnaDB1920.DBO.tblSMSSent s
+			  WHERE s.CreatedAt BETWEEN @FDT AND @TDT	
+              GROUP BY s.UserID 
+              ) T2 ON t2.userId = t1.UserID 
+              WHERE t1.USerID = 37
+
+UNION
+
+-- select * from ScholarDB1920.dbo.tblUserInfo
+
+SELECT t1.userID, 'ScholarDB1920' School
+, purchased
+, IsNull(smsSent, 0) SMSSent 
+, T1.Purchased - IsNull(smsSent, 0) as BalanceOnHand 
+              FROM 
+             ( 
+              select userID, SUM(SmsPurchasedCount) Purchased 
+              FROM ScholarDB1920.DBO.tblSMSPurchased p
+			  WHERE p.CreatedAt BETWEEN @FDT AND @TDT	
+              GROUP BY p.UserID 
+              ) T1  
+              LEFT OUTER JOIN  
+              ( 
+              select userID, SUM(msgCount) SMSSent 
+              FROM ScholarDB1920.DBO.tblSMSSent s
+			  WHERE s.CreatedAt BETWEEN @FDT AND @TDT	
+              GROUP BY s.UserID 
+              ) T2 ON t2.userId = t1.UserID 
+              WHERE t1.USerID = 36
 
