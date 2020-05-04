5f2244e15fa0c7f8ddd5b32ee16b4ff1108ad03e
Sandeep2314 <CollegeManager@gmail.com>
2020-05-04 10:49:58 +0530

    added menu items

--- app/src/main/java/com/example/eklass/BaseActivity.java
+++ app/src/main/java/com/example/eklass/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.example.eklass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,6 +26,34 @@ public class BaseActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
+
+        User usr = SharedPrefManager.getInstance(this).getUser();
+
+        Log.w("Sandeep Menu", "usr.getStaffType() 333 " + usr.getStaffType());
+
+        if(usr.getStaffType().equals("1"))
+        {
+           //MenuItem addStaff =   findViewById(R.id.menuAddStaff);
+           MenuItem addStaff =   menu.findItem(R.id.menuAddStaff);
+           MenuItem showStaff  = menu.findItem(R.id.menuShowStaff);
+           MenuItem addLocations = menu.findItem(R.id.menuAddLocations);
+           MenuItem showLocations =   menu.findItem(R.id.menuShowLocations);
+
+
+           if(addStaff != null) {
+               addStaff.setVisible(false);
+               showStaff.setVisible(false);
+               addLocations.setVisible(false);
+               showLocations.setVisible(false);
+
+               Log.w("Sandeep Menu", "usr.getStaffType() 444 " + usr.getStaffType());
+           }
+            Log.w("Sandeep Menu", "usr.getStaffType() 555 " + usr.getStaffType());
+
+
+        }
+
+
         return true;
     }
 
@@ -32,11 +62,26 @@ public class BaseActivity extends AppCompatActivity {
 
         switch (item.getItemId())
         {
+            case R.id.menuAddStaff:
+                finish();
+                startActivity( new Intent(this, AddStaffActivity.class));
+                return true;
+            case R.id.menuShowStaff:
+                finish();
+                startActivity( new Intent(this, ManagerDashboardActivity.class));
+                return true;
+            case  R.id.menuAddLocations:
+                finish();
+                startActivity( new Intent(this, LocationActivity.class));
+            case R.id.menuShowLocations:
+                Toast.makeText(this, "Show Locations Clicked", Toast.LENGTH_SHORT).show();
+                return true;
             case R.id.menuLogOut:
                 Toast.makeText(this, "Log out Clicked", Toast.LENGTH_SHORT).show();
                 finish();
                 SharedPrefManager.getInstance(getApplicationContext()).logout();
                 return true;
+
             default: return super.onOptionsItemSelected(item);
         }
 
