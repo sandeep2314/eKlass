e83ae80752e5b215ed7927b85c4593afaedac5e4
Sandeep2314 <CollegeManager@gmail.com>
2020-11-20 12:04:18 +0530

    added monthsheet

--- app/src/main/java/com/example/eklass/TimeSheet.java
+++ app/src/main/java/com/example/eklass/TimeSheet.java
@@ -8,15 +8,74 @@ public class TimeSheet {
     public String Hierarchy;
 
     public DEY DAY1;
+    public DEY DAY2;
+    public DEY DAY3;
+    public DEY DAY4;
+    public DEY DAY5;
+    public DEY DAY6;
+    public DEY DAY7;
+    public DEY DAY8;
+    public DEY DAY9;
+    public DEY DAY10;
+    public DEY DAY11;
+    public DEY DAY12;
+    public DEY DAY13;
+    public DEY DAY14;
+    public DEY DAY15;
+    public DEY DAY16;
+    public DEY DAY17;
+    public DEY DAY18;
+    public DEY DAY19;
+    public DEY DAY20;
+    public DEY DAY21;
+    public DEY DAY22;
+    public DEY DAY23;
+    public DEY DAY24;
+    public DEY DAY25;
     public DEY DAY26;
+    public DEY DAY27;
+    public DEY DAY28;
+    public DEY DAY29;
+    public DEY DAY30;
+    public DEY DAY31;
 
-    public TimeSheet(String staffId, String staffName, String designation, String hierarchy, DEY DAY1, DEY DAY26) {
+
+    public TimeSheet(String staffId, String staffName, String designation, String hierarchy, DEY DAY1, DEY DAY2, DEY DAY3, DEY DAY4, DEY DAY5, DEY DAY6, DEY DAY7, DEY DAY8, DEY DAY9, DEY DAY10, DEY DAY11, DEY DAY12, DEY DAY13, DEY DAY14, DEY DAY15, DEY DAY16, DEY DAY17, DEY DAY18, DEY DAY19, DEY DAY20, DEY DAY21, DEY DAY22, DEY DAY23, DEY DAY24, DEY DAY25, DEY DAY26, DEY DAY27, DEY DAY28, DEY DAY29, DEY DAY30, DEY DAY31) {
         StaffId = staffId;
         StaffName = staffName;
         Designation = designation;
         Hierarchy = hierarchy;
         this.DAY1 = DAY1;
+        this.DAY2 = DAY2;
+        this.DAY3 = DAY3;
+        this.DAY4 = DAY4;
+        this.DAY5 = DAY5;
+        this.DAY6 = DAY6;
+        this.DAY7 = DAY7;
+        this.DAY8 = DAY8;
+        this.DAY9 = DAY9;
+        this.DAY10 = DAY10;
+        this.DAY11 = DAY11;
+        this.DAY12 = DAY12;
+        this.DAY13 = DAY13;
+        this.DAY14 = DAY14;
+        this.DAY15 = DAY15;
+        this.DAY16 = DAY16;
+        this.DAY17 = DAY17;
+        this.DAY18 = DAY18;
+        this.DAY19 = DAY19;
+        this.DAY20 = DAY20;
+        this.DAY21 = DAY21;
+        this.DAY22 = DAY22;
+        this.DAY23 = DAY23;
+        this.DAY24 = DAY24;
+        this.DAY25 = DAY25;
         this.DAY26 = DAY26;
+        this.DAY27 = DAY27;
+        this.DAY28 = DAY28;
+        this.DAY29 = DAY29;
+        this.DAY30 = DAY30;
+        this.DAY31 = DAY31;
     }
 
     public String getStaffId() {
@@ -39,7 +98,123 @@ public class TimeSheet {
         return DAY1;
     }
 
+    public DEY getDAY2() {
+        return DAY2;
+    }
+
+    public DEY getDAY3() {
+        return DAY3;
+    }
+
+    public DEY getDAY4() {
+        return DAY4;
+    }
+
+    public DEY getDAY5() {
+        return DAY5;
+    }
+
+    public DEY getDAY6() {
+        return DAY6;
+    }
+
+    public DEY getDAY7() {
+        return DAY7;
+    }
+
+    public DEY getDAY8() {
+        return DAY8;
+    }
+
+    public DEY getDAY9() {
+        return DAY9;
+    }
+
+    public DEY getDAY10() {
+        return DAY10;
+    }
+
+    public DEY getDAY11() {
+        return DAY11;
+    }
+
+    public DEY getDAY12() {
+        return DAY12;
+    }
+
+    public DEY getDAY13() {
+        return DAY13;
+    }
+
+    public DEY getDAY14() {
+        return DAY14;
+    }
+
+    public DEY getDAY15() {
+        return DAY15;
+    }
+
+    public DEY getDAY16() {
+        return DAY16;
+    }
+
+    public DEY getDAY17() {
+        return DAY17;
+    }
+
+    public DEY getDAY18() {
+        return DAY18;
+    }
+
+    public DEY getDAY19() {
+        return DAY19;
+    }
+
+    public DEY getDAY20() {
+        return DAY20;
+    }
+
+    public DEY getDAY21() {
+        return DAY21;
+    }
+
+    public DEY getDAY22() {
+        return DAY22;
+    }
+
+    public DEY getDAY23() {
+        return DAY23;
+    }
+
+    public DEY getDAY24() {
+        return DAY24;
+    }
+
+    public DEY getDAY25() {
+        return DAY25;
+    }
+
     public DEY getDAY26() {
         return DAY26;
+    }
+
+    public DEY getDAY27() {
+        return DAY27;
+    }
+
+    public DEY getDAY28() {
+        return DAY28;
+    }
+
+    public DEY getDAY29() {
+        return DAY29;
+    }
+
+    public DEY getDAY30() {
+        return DAY30;
+    }
+
+    public DEY getDAY31() {
+        return DAY31;
     }
 }
